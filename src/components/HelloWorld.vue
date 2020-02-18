99662fe163446b99150cc7e6614d2fc102f46cf6
jackycheung <3209669536@qq.com>
2020-02-18 11:37:30 +0800

    项目初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Button } from "element-ui";
+Vue.use(Button);
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
